5da6238003289266b7c5661a50af4964b00b569b
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-06-19 23:38:02 +0300

    Fixed require lib

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
  * AWS CREDENTIALS SHOULD BE IN ENV VARIABLES
  */
 
-var path = require('path');
-var fs   = require('fs');
-
 /**
  *
  * @constructor
@@ -13,15 +10,6 @@ var Aws_wrapper = function () {
   this.name = 'XAws';
 };
 
-var normalizedPath = path.join(__dirname, 'lib');
-fs.readdirSync(normalizedPath).forEach(function (fileNameWithExt) {
-  if (fs.statSync(path.join(normalizedPath, fileNameWithExt)).isFile()) {
-    require("./" + fileNameWithExt)(Aws_wrapper);
-  }
-  else {
-    require("./" + fileNameWithExt + '/index.js')(Aws_wrapper);
-  }
-
-});
+require('./lib/index')(Aws_wrapper);
 
 exports._aws_wrapper = new Aws_wrapper();
